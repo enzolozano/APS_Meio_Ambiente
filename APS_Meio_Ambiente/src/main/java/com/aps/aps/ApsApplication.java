672f5e731d395070650ffdd9644da4ab644db6f1
enzo_lozano <enzo.malozano@gmail.com>
2023-04-18 14:53:38 -0300

    Recria projeto novamente

--- APS_Meio_Ambiente/src/main/java/com/aps/aps/ApsApplication.java
+++ APS_Meio_Ambiente/src/main/java/com/aps/aps/ApsApplication.java
@@ -1,21 +1,13 @@
-package com.project.APS;
+package com.aps.aps;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.TimeZone;
 
 @SpringBootApplication
 public class ApsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApsApplication.class, args);
-	}
-
-	@PostConstruct
-	public void init() {
-		TimeZone.setDefault(TimeZone.getTimeZone(("America/Sao_Paulo")));
 	}
 
 }
