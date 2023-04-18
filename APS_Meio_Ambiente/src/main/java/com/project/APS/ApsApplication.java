1dccefee4007d958a85b415286564c8e46306dec
enzo_lozano <enzo.malozano@gmail.com>
2023-04-18 12:29:44 -0300

    Adiciona TimeZone para o projeto

--- APS_Meio_Ambiente/src/main/java/com/project/APS/ApsApplication.java
+++ APS_Meio_Ambiente/src/main/java/com/project/APS/ApsApplication.java
@@ -1,13 +1,21 @@
 package com.project.APS;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class ApsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApsApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone(("America/Sao_Paulo")));
 	}
 
 }
