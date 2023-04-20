96e2b31a4433c0c746c9d17b9ec61bc43a308379
enzo_lozano <enzo.malozano@gmail.com>
2023-04-20 08:38:57 -0300

    :sparkles:  Insere datetime no springboot

--- APS_Meio_Ambiente/src/main/java/com/aps/aps/ApsApplication.java
+++ APS_Meio_Ambiente/src/main/java/com/aps/aps/ApsApplication.java
@@ -1,13 +1,21 @@
 package com.aps.aps;
 
+import java.util.TimeZone;
+
+import jakarta.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class ApsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApsApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault((TimeZone.getTimeZone("America/Sao_Paulo")));
 	}
 
 }
