c1ee1d650679b955117fe134b0a17baa3b9d571c
enzo_lozano <enzo.malozano@gmail.com>
2023-05-22 17:34:16 -0300

    :sparkles: Adapta mapper para suportar escrita vindo da rota de medidas

--- APS_Meio_Ambiente/src/main/java/com/aps/aps/Mappers/ParameterMapper.java
+++ APS_Meio_Ambiente/src/main/java/com/aps/aps/Mappers/ParameterMapper.java
@@ -7,29 +7,29 @@ public class ParameterMapper {
     public String ParticleToDescriptionPT(String particle) {
         Map<String, String> particles = new HashMap<>();
 
-        particles.put("PM10", "Matéria particulada com menos de 10 micrômetros de concentração de massa de diâmetro.");
-        particles.put("PM2.5", "Matéria particulada com menos de 2.5 micrômetros de concentração de massa de diâmetro.");
-        particles.put("PM1", "Matéria particulada com menos de 1 micrômetros de concentração de massa de diâmetro");
-        particles.put("PM4", "Matéria particulada com menos de 4 micrômetros de concentração de massa de diâmetro");
-        particles.put("O₃ mass", "Concentração de massa de ozônio");
-        particles.put("CO mass", "Concentração de massa de monóxido de carbono");
-        particles.put("NO₂ mass", "Concentração de massa de dióxido de nitrôgenio");
-        particles.put("SO₂ mass", "Concentração de massa de dióxido de enxofre");
-        particles.put("NOx mass", "Concentração de massa de dióxido de nitrôgenio");
-        particles.put("NO₂", "Concentração de dióxido de nitrôgenio");
-        particles.put("CO", "Concentração de monóxido de carbono");
-        particles.put("SO₂", "Concentração de dióxido de enxofre");
-        particles.put("O₃", "Concentração de ozônio");
-        particles.put("BC", "Concentração de massa de carbono negro");
-        particles.put("CO₂", "Concentração de dióxido de carbono");
-        particles.put("CH₄", "Concentração de metano");
-        particles.put("UFP count", "Concentração de quantidade de partículas ultrafinas");
-        particles.put("NO", "Concentração de monóxido de nitrôgenio");
-        particles.put("PM1 count", "Quantidade de PM1");
-        particles.put("PM2.5 count", "Quantidade de PM2.5");
-        particles.put("PM10 count", "Quantidade de PM10");
-        particles.put("NOx", "Concentração de óxido de nitrôgenio");
-        particles.put("NO mass", "Concentração de massa de monóxido de nitrôgenio");
+        particles.put("pm10", "Matéria particulada com menos de 10 micrômetros de concentração de massa de diâmetro.");
+        particles.put("pm25", "Matéria particulada com menos de 2.5 micrômetros de concentração de massa de diâmetro.");
+        particles.put("pm1", "Matéria particulada com menos de 1 micrômetros de concentração de massa de diâmetro.");
+        particles.put("pm4", "Matéria particulada com menos de 4 micrômetros de concentração de massa de diâmetro.");
+        particles.put("o3 mass", "Concentração de massa de ozônio.");
+        particles.put("CO mass", "Concentração de massa de monóxido de carbono.");
+        particles.put("NO₂ mass", "Concentração de massa de dióxido de nitrôgenio.");
+        particles.put("SO₂ mass", "Concentração de massa de dióxido de enxofre.");
+        particles.put("NOx mass", "Concentração de massa de dióxido de nitrôgenio.");
+        particles.put("no2", "Concentração de dióxido de nitrôgenio.");
+        particles.put("co", "Concentração de monóxido de carbono.");
+        particles.put("so2", "Concentração de dióxido de enxofre.");
+        particles.put("o3", "Concentração de ozônio.");
+        particles.put("bc", "Concentração de massa de carbono negro.");
+        particles.put("co2", "Concentração de dióxido de carbono.");
+        particles.put("ch4", "Concentração de metano.");
+        particles.put("UFP count", "Concentração de quantidade de partículas ultrafinas.");
+        particles.put("no", "Concentração de monóxido de nitrôgenio.");
+        particles.put("PM1 count", "Quantidade de PM1.");
+        particles.put("PM2.5 count", "Quantidade de PM2.5.");
+        particles.put("PM10 count", "Quantidade de PM10.");
+        particles.put("NOx", "Concentração de óxido de nitrôgenio.");
+        particles.put("NO mass", "Concentração de massa de monóxido de nitrôgenio.");
 
         String description = "";
 
