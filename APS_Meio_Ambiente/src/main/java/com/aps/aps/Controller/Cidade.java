9a40340e03b65a4f80330b06b22c3f435fe28f2f
enzo_lozano <enzo.malozano@gmail.com>
2023-05-22 17:29:43 -0300

    :sparkles: Remove rota adicionada anteriormente

--- APS_Meio_Ambiente/src/main/java/com/aps/aps/Controller/Cidade.java
+++ APS_Meio_Ambiente/src/main/java/com/aps/aps/Controller/Cidade.java
@@ -2,13 +2,11 @@ package com.aps.aps.Controller;
 
 import com.aps.aps.ControllerRequest.GerenciaAPI;
 import com.aps.aps.Helpers.FuncBasicas;
-import com.aps.aps.Model.MaiorSubstanciaModel;
 import com.aps.aps.Model.MedidaModel;
 import com.aps.aps.Model.RespostaModel;
 import com.aps.aps.ResponseModel.CitiesResponse;
 import com.aps.aps.ResponseModel.LatestMeasurementResponse;
 
-import com.aps.aps.ResponseModel.MeasurementsList;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -17,23 +15,23 @@ import com.google.gson.Gson;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping(value = "/Cidade")
 public class Cidade {
 
     private final FuncBasicas _funcao = new FuncBasicas();
 
-    @RequestMapping(value = "/MedidaMaisRecente", method = RequestMethod.GET)
-    public ResponseEntity<?> MedidaMaisRecente(String city) {
+    @RequestMapping(value = "/Medidas", method = RequestMethod.GET)
+    public ResponseEntity<RespostaModel> Medidas(String city) {
 
         String URL = "/v2/latest?sort=desc&country_id=BR&city=" + city +"&order_by=lastUpdated&dumpRaw=false";
 
@@ -69,51 +67,6 @@ public class Cidade {
 
             return new ResponseEntity<RespostaModel>(_funcao.MontaModeloResposta("FALHA", ex.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
         }
-    }
-
-    @RequestMapping(value = "/MaiorSubstancia", method = RequestMethod.GET)
-    public ResponseEntity<?> MaiorSubstancia(String city) {
-
-        String URL = "/v2/latest?sort=desc&country_id=BR&city=" + city +"&order_by=lastUpdated&dumpRaw=false";
-
-        String resposta = new GerenciaAPI().GET(URL);
-
-        if (resposta.length() == 0) {
-
-            return new ResponseEntity<>(_funcao.MontaModeloResposta("FALHA", "Não foi possível receber resposta da requisição", null), HttpStatus.NOT_FOUND);
-        }
-
-        LatestMeasurementResponse lmr = new LatestMeasurementResponse();
-        ObjectMapper mapper = new ObjectMapper();
-
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-
-        try {
-
-            lmr = new Gson().fromJson(resposta, LatestMeasurementResponse.class);
-
-            if (lmr.getResults().isEmpty()) {
-
-                return new ResponseEntity<>(_funcao.MontaModeloResposta("FALHA", "Nenhum registro encontrado", null), HttpStatus.NO_CONTENT);
-            }
-
-            MaiorSubstanciaModel maiorSubstanciaModel = new MaiorSubstanciaModel();
-
-            maiorSubstanciaModel.setCidade(lmr.getResults().get(0).getCity());
-            maiorSubstanciaModel.setMedida(ordenar(lmr.getResults().get(0).getMeasurements()).get(0));
-
-            return new ResponseEntity<RespostaModel>(_funcao.MontaModeloResposta("OK", "", maiorSubstanciaModel), HttpStatus.OK);
-        }
-        catch (Exception ex) {
-
-            return new ResponseEntity<RespostaModel>(_funcao.MontaModeloResposta("FALHA", ex.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    public List<MeasurementsList> ordenar (List<MeasurementsList> lista) {
-        return lista.stream()
-                .sorted(Comparator.comparingDouble(MeasurementsList::getValue).reversed())
-                .collect(Collectors.toList());
     }
 
     @RequestMapping(value = "/Lista", method = RequestMethod.GET)
