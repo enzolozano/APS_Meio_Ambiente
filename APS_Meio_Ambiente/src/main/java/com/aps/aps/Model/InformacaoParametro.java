6a7647e2d87578d58fd2424f03d202d09637651d
enzo_lozano <enzo.malozano@gmail.com>
2023-05-22 17:35:01 -0300

    :sparkles: Cria model para informações de parametro

---
+++ APS_Meio_Ambiente/src/main/java/com/aps/aps/Model/InformacaoParametro.java
@@ -0,0 +1,22 @@
+package com.aps.aps.Model;
+
+public class InformacaoParametro {
+    public String nome;
+    public String descricao;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
